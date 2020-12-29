31d1632dcc24513868f3d8385ec11d96a50d7fa1
Da0hn <gabrielhondacba@gmail.com>
2020-12-29 11:50:33 -0400

    feat: adiciona ListView.builder ao componente TransactionList

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,55 +10,60 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: this.transactions.map((transaction) {
-        return Card(
-          child: Row(
-            children: [
-              Container(
-                child: Text(
-                  'R\$ ${transaction.value.toStringAsFixed(2)}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.deepOrange,
-                  ),
-                ),
-                margin: EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 10,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purpleAccent,
-                    width: 2,
-                  ),
-                ),
-                padding: EdgeInsets.all(10),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    transaction.title,
+    return Container(
+      height: 350,
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (_, index) {
+          final transaction = transactions[index];
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  child: Text(
+                    'R\$ ${transaction.value.toStringAsFixed(2)}',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                      fontSize: 20,
                       color: Colors.deepOrange,
                     ),
                   ),
-                  Text(
-                    DateFormat('dd MMM yyyy').format(transaction.date),
-                    style: TextStyle(
-                      color: Colors.greenAccent,
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purpleAccent,
+                      width: 2,
                     ),
                   ),
-                ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                  padding: EdgeInsets.all(10),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      transaction.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.deepOrange,
+                      ),
+                    ),
+                    Text(
+                      DateFormat('dd MMM yyyy').format(transaction.date),
+                      style: TextStyle(
+                        color: Colors.greenAccent,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
