92b98924278936e60aa8c2ae155c908681cd5d54
Da0hn <gabrielhondacba@gmail.com>
2020-12-30 09:51:06 -0400

    refactor: cria Widget HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'components/transaction_user.dart';
+import 'components/home_page.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -12,38 +12,6 @@ class ExpensesApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [IconButton(icon: Icon(Icons.add), onPressed: null)],
-        title: Text('Despesas Pessoais'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              child: Card(
-                color: Colors.deepPurpleAccent,
-                elevation: 5,
-                child: Text('Gráfico'),
-              ),
-            ),
-            TransactionUser(),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: null,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
