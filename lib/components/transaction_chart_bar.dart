7f1fa21ada290bcf3ab2675ccfd7eeb69d7e10dc
Gabriel Honda <gabrielhondacba@gmail.com>
2020-12-30 16:23:44 -0400

    style: melhora responsividade do layout

--- lib/components/transaction_chart_bar.dart
+++ lib/components/transaction_chart_bar.dart
@@ -15,39 +15,45 @@ class TransactionChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('${this.value.toStringAsFixed(2)}'),
-        SizedBox(height: 5),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.deepPurpleAccent,
-                    width: 1.0,
-                  ),
-                  color: Color(0xFF91C593),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: percentage,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurpleAccent,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
-              ),
-            ],
-          ),
+        FittedBox(
+          child: Text('${this.value.toStringAsFixed(2)}'),
         ),
+        SizedBox(height: 5),
+        _bar(),
         SizedBox(height: 5),
         Text(this.label),
       ],
+    );
+  }
+
+  Widget _bar() {
+    return Container(
+      height: 60,
+      width: 10,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.deepPurpleAccent,
+                width: 1.0,
+              ),
+              color: Color(0xFF91C593),
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+          FractionallySizedBox(
+            heightFactor: percentage,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
