f5e3e42dec8890cee84b3e1fa8eddda91be74eb4
Gabriel Honda <gabrielhondacba@gmail.com>
2020-12-30 19:44:14 -0400

    fix: ajuste na largura da tela

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -14,7 +14,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 500,
       child: _transactions.isEmpty
           ? _noTransactionFound(context)
           : _transactionList(context),
