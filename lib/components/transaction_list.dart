f446d1ffb05bbdc9048459e33716994e2d24b94a
Da0hn <gabrielhondacba@gmail.com>
2020-12-30 11:06:37 -0400

    feat: adiciona imagem e mensagem quando a lista de transações estiver vazia

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -12,54 +12,80 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 350,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (_, index) {
-          final transaction = transactions[index];
-          return Card(
-            child: Row(
-              children: [
-                Container(
-                  child: Text(
-                    'R\$ ${transaction.value.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.deepOrange,
-                    ),
+      child: transactions.isEmpty
+          ? _noTransactionFound(context)
+          : _transactionList(context),
+    );
+  }
+
+  ListView _transactionList(BuildContext context) {
+    return ListView.builder(
+      itemCount: transactions.length,
+      itemBuilder: (_, index) {
+        final transaction = transactions[index];
+        return Card(
+          child: Row(
+            children: [
+              Container(
+                child: Text(
+                  'R\$ ${transaction.value.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.deepOrange,
                   ),
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
-                    ),
-                  ),
-                  padding: EdgeInsets.all(10),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      transaction.title,
-                      style: Theme.of(context).textTheme.headline6,
+                margin: EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Theme.of(context).primaryColor,
+                    width: 2,
+                  ),
+                ),
+                padding: EdgeInsets.all(10),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    transaction.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  Text(
+                    DateFormat('dd MMM yyyy').format(transaction.date),
+                    style: TextStyle(
+                      color: Colors.blueGrey,
                     ),
-                    Text(
-                      DateFormat('dd MMM yyyy').format(transaction.date),
-                      style: TextStyle(
-                        color: Colors.blueGrey,
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          );
-        },
-      ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Column _noTransactionFound(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 20),
+        Text(
+          'Nenhuma transação cadastrada!',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        SizedBox(height: 20),
+        Container(
+          height: 200,
+          child: Image.asset(
+            'assets/images/waiting.png',
+            fit: BoxFit.fitHeight,
+          ),
+        )
+      ],
     );
   }
 }
